09907bb37a9ea27983d8b17c72cd4977ba257a61
PatrickC05 <53089989+PatrickC05@users.noreply.github.com>
2020-03-27 12:19:47 -0400

    Update Firstidea.py

--- Firstidea.py
+++ Firstidea.py
@@ -3,6 +3,7 @@ First (Ratio) Idea
 We'll probably have to do noise reduction
 """
 import numpy as np
+#testvals- feel free to change
 first=np.ones((10,5))
 second=np.ones((10,5))
 second[4:8,1]=1.5
