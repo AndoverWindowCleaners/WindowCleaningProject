ebdeeccf5fe1d2d3f6d666d0347a4439002af348
PatrickC05 <53089989+PatrickC05@users.noreply.github.com>
2020-03-27 11:48:23 -0400

    Ratios

---
+++ Firstidea.py
@@ -0,0 +1,18 @@
+"""
+First (Ratio) Idea
+We'll probably have to do noise reduction
+"""
+import numpy as np
+first=np.ones((10,5))
+second=np.ones((10,5))
+second[4:8,1]=1.5
+def isWindow(First,Second,epsilon):
+    """
+    First and Second are numpy arrays of images
+    Epsilon is a float (>=1) to determine whether a pixel has a window or not
+    Function uses 1/epsilon also
+    
+    Returns True for window pixels, false otherwise
+    """
+    ratios=First/Second
+    return np.logical_or(ratios<=(1/epsilon),ratios>=epsilon)
