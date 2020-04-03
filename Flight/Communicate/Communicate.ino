a685668410807f2b08e4d7e5bd030590969e9607
PatrickC05 <53089989+PatrickC05@users.noreply.github.com>
2020-04-03 08:59:03 -0400

    Create Communicate.ino

---
+++ Flight/Communicate/Communicate.ino
@@ -0,0 +1,11 @@
+//Not entirely sure if this is necessary, but I created it just in case
+//Stuff in this program should communicate (i/o) with raspberry pi
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
