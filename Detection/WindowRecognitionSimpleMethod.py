1e44963bf3931fcdefa89c54b744ca00996c6e0f
michaelyhuang23 <55302647+michaelyhuang23@users.noreply.github.com>
2020-10-03 18:08:29 -0400

    Update WindowRecognitionSimpleMethod.py

--- Detection/WindowRecognitionSimpleMethod.py
+++ Detection/WindowRecognitionSimpleMethod.py
@@ -11,13 +11,19 @@ import math
 import cv2
 import time
 
-num_frame = 60
-frame_width=1280
-frame_height=1280
-input_frames = np.zeros((num_frame,frame_width,frame_height), dtype = np.float32)
-derivative1 = np.zeros((num_frame,frame_width,frame_height), dtype = np.float32)
-derivative2_corrected = np.zeros((num_frame,frame_width,frame_height), dtype = np.float32)
-differences = np.zeros((num_frame,frame_width,frame_height), dtype = np.float32)
+num_frame = 600
+frame_width = 128
+frame_height = 128
+target_frame_width = 28
+target_frame_height = 28
+input_frames = np.zeros(
+    (2, frame_width, frame_height), dtype=np.float32)
+derivative1 = np.zeros(
+    (2, frame_width, frame_height), dtype=np.float32)
+cur_derivative2_corrected = np.zeros(
+    (frame_width, frame_height), dtype=np.float32)
+differences = np.zeros(
+    (num_frame, frame_width, frame_height), dtype=np.float32)
 # note that these numpy arrays are used as cyclic arrays
 start_frame = 0
 rotation_frequency = 1  # enter in revolution per second
@@ -26,7 +32,7 @@ rotation_frequency = 1  # enter in revolution per second
 # its derivative is a*b*cos(bx+c)
 # its second derivative is -a*b*b*sin(bx+c)
 # so f(x)-f'(x-pi/(2*b))/b should be constant so should f(x)+f"(x)/b/b
-# the second is much better because the first involves a phase shift which 
+# the second is much better because the first involves a phase shift which
 # requires access to data taken pi/(2*b) ago and will thus lower the processing
 # speed or accuracy.
 # the key is to know b, which is equal to frequency*2pi
@@ -36,52 +42,48 @@ frequency_const = rotation_frequency*2*math.pi
 difference_sum = sum(differences)
 difference_square_sum = sum([i**2 for i in differences])
 
+
 def get_new_frame():    # to be implemented in coordination with the camera
     pass
 
-def image_pooling(image, half_box_size, stride):    
-    # avery poor implementation, to be replaced
-    # by prefix sum or rolling implementation
-    return cv2.resize(image,(frame_width, frame_height),interpolation=cv2.INTER_AREA)
-    # newImage = np.zeros((int((len(image)-half_box_size*2+1)/stride),int((len(image[0])-half_box_size*2+1)/stride)),dtype='float')
-    # ri=0
-    # for row in range(half_box_size,len(image)-half_box_size,stride):
-    #     ci=0
-    #     for col in range(half_box_size,len(image[0])-half_box_size,stride):
-    #         cutoutImage = image[(row-half_box_size):(row+half_box_size+1),(col-half_box_size):(col+half_box_size+1)]
-    #         newImage[ri,ci] = sum(sum(cutoutImage))/(4*half_box_size**2)
-    #         ci+=1
-    #     ri+=1
-            
+
+def image_pooling(image, new_width, new_height):
+    return cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_AREA)
+
+
 def find_block(image):  # to be implemented
     pass
+
+
 count = 1
 while True:
     if count % 10 == 0:
-        try:
-            print(time.time()-start)
-        except:
-            pass
+        print(time.time()-start)
         start = time.time()
-    
-    input_frames[start_frame] =  get_new_frame()
-    delta_time = 1  # get the delta time using api provided by raspberry PI or arduino
-    derivative1[start_frame] = (input_frames[start_frame]-input_frames[start_frame-1])/delta_time
-    
-    start_frame+=1
-    if(start_frame>=num_frame):
-        start_frame-=num_frame
-    
-    derivative2_corrected[start_frame-1] = (derivative1[start_frame]-derivative1[start_frame-1])/delta_time
-    derivative2_corrected[start_frame-1]/=frequency_const**2
-    difference_sum-=differences[start_frame-1]
-    difference_square_sum-=differences[start_frame-1]**2
-    differences[start_frame-1]=derivative2_corrected[start_frame-1]+input_frames[start_frame-1]
+
+    input_frames[start_frame%2] = image_pooling(get_new_frame(),frame_width,frame_height)
+    delta_time = 1.0  # get the delta time using api provided by raspberry PI or arduino
+    derivative1[start_frame%2] = (
+        input_frames[start_frame%2]-input_frames[(start_frame-1)%2])/delta_time
+
+    cur_derivative2_corrected = (derivative1[start_frame%2]-derivative1[(start_frame-1)%2])/delta_time
+    cur_derivative2_corrected /= frequency_const**2
+
+
+
+    difference_sum -= differences[start_frame-1]
+    difference_square_sum -= differences[start_frame-1]**2
+    differences[start_frame-1] = cur_derivative2_corrected + \
+        input_frames[start_frame-1]
+    difference_sum += differences[start_frame-1]
+    difference_square_sum += differences[start_frame-1]**2
     variances = (difference_square_sum/num_frame-(difference_sum/num_frame)**2)
     # note this is only an estimation of variance, not the actual variance, which may be difficult
     # to evaluate on a rolling basis
-    
-    variances = image_pooling(variances,1,2)
+
+    start_frame += 1
+    start_frame %= num_frame
+
+    variances = image_pooling(variances, target_frame_width, target_frame_height)
     find_block(variances)
-    count +=1
-    
+    count += 1
