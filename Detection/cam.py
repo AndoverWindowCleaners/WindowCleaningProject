274aa876682d7b910ab4c73f0aed9e087eea6a47
PatrickC05 <53089989+PatrickC05@users.noreply.github.com>
2020-12-11 16:08:24 -0500

    Create cam.py

---
+++ Detection/cam.py
@@ -0,0 +1,16 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+camera = PiCamera()
+camera.resolution = (640,480)
+camera.framerate = 15
+rawCapture = PiRGBArray(camera, size=(640,480))
+time.sleep(0.1)
+for frame in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
+	image = frame.array
+	cv2.imshow("Stream", image)
+	key = cv2.waitKey(1) & 0xFF
+	rawCapture.truncate(0)
+	if key == ord("q"):
+		break
