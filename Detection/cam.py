160cdc471a35a1968be03f132cab1f02d22c1065
Michael Huang <michaelyhuang23@gmail.com>
2021-04-25 23:37:19 -0400

    data collection code

--- Detection/cam.py
+++ Detection/cam.py
@@ -7,12 +7,19 @@ camera.framerate = 30
 rawCapture = PiRGBArray(camera, size=(128, 96))
 print(camera.framerate_range.high)
 time.sleep(0.1)
+
+fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
+video_filename = 'indoor.avi'
+out = cv2.VideoWriter(video_filename, fourcc, 30, (128, 96))
+
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     image = frame.array
     cv2.imshow("Stream", image)
+    out.write(image)
     rawCapture.truncate(0)
     key = cv2.waitKey(1) & 0xFF
     if key == ord("q"):
         break
 
 cv2.destroyAllWindows()
+out.release()
