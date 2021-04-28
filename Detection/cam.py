479bef7de3227f36d8f279e060cf441644d21ea9
Michael Huang <michaelyhuang23@gmail.com>
2021-04-27 23:57:52 -0400

    window data and new cam file

--- Detection/cam.py
+++ Detection/cam.py
@@ -1,3 +1,6 @@
+with open('/home/pi/Desktop/runOnStartUp/printer.txt','w') as b:
+    b.write('hello225')
+
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
@@ -5,21 +8,24 @@ import cv2
 camera = PiCamera(resolution=(128, 96), framerate=30)
 camera.framerate = 30
 rawCapture = PiRGBArray(camera, size=(128, 96))
-print(camera.framerate_range.high)
 time.sleep(0.1)
 
 fourcc = cv2.VideoWriter_fourcc('M','J','P','G')
-video_filename = 'indoor.avi'
+video_filename = '/home/pi/Desktop/runOnStartUp/indoor.avi'
 out = cv2.VideoWriter(video_filename, fourcc, 30, (128, 96))
+
+time.sleep(3)
+
+start = time.time()
 
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
     image = frame.array
-    cv2.imshow("Stream", image)
+    #cv2.imshow("Stream", image)
     out.write(image)
     rawCapture.truncate(0)
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord("q"):
+    #key = cv2.waitKey(1) & 0xFF
+    if time.time()-start>30:
         break
 
-cv2.destroyAllWindows()
+#cv2.destroyAllWindows()
 out.release()
