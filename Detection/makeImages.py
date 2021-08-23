6f0b3b7518df356d443489acaa75d8b30fc534ab
PatrickC05 <53089989+PatrickC05@users.noreply.github.com>
2021-08-22 21:43:58 -0400

    all image folders added

--- Detection/makeImages.py
+++ Detection/makeImages.py
@@ -1,34 +1,39 @@
 import cv2
 import os
 
-
-EVERY_OTHER = 10
-
-name = '20210710-195253.avi'
-
-file_name = 'training_videos/' + name
-
-new_folder = 'images/' + name
-
-print(new_folder)
-
-if not os.path.exists(new_folder):
-    os.makedirs(new_folder)
-
-cap = cv2.VideoCapture(file_name)
-
-i = 0
-num_images = 0
-
-while cap.isOpened():
-    ret, frame = cap.read()
-    if i == 0:
-        new_name = "%04d" % num_images
-        cv2.imwrite(new_folder + '/' + new_name + '.jpg', frame)
-        num_images += 1
-
-    i = (i + 1) % EVERY_OTHER
+for name in os.listdir('training_videos'):
 
 
-cap.release()
+    EVERY_OTHER = 10
+
+    # name = '20210710-195752.avi'
+
+    file_name = 'training_videos/' + name
+
+    new_folder = 'images/' + name
+
+    print(new_folder)
+
+    if not os.path.exists(new_folder):
+        os.makedirs(new_folder)
+
+    cap = cv2.VideoCapture(file_name)
+
+    i = 0
+    num_images = 0
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret:
+            if i == 0:
+                new_name = "%04d" % num_images
+                cv2.imwrite(new_folder + '/' + new_name + '.jpg', frame)
+                num_images += 1
+        else:
+            break
+
+        i = (i + 1) % EVERY_OTHER
+
+
+    cap.release()
 cv2.destroyAllWindows()
